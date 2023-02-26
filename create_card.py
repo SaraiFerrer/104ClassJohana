1904c3e841cab7c062f2caea73234ea73ca71e5c
SaraiFerrer <87380666+SaraiFerrer@users.noreply.github.com>
2023-02-26 13:43:41 -0600

    Add files via upload

---
+++ create_card.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+
+
+cv2.putText(img,  
+           text_to_show,
+           (20, 220),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=1,  
+           color=(0,0,255)
+           )
+
+cv2.imshow("Output",img)
+
+
+cv2.waitKey(0)
